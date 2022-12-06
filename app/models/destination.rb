e74cc1d6d1181d43e76deae91ee95a35610d2179
wanjiku <annritawanjiku77@gmail.com>
2022-12-06 14:35:50 +0300

    added associations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,10 @@
 class Destination < ApplicationRecord
+
+    belongs_to :user
+    has_many :reviews
+
+
+    
     validates :destination_name, presence: true
     validates :location, presence: true
     validates :cost, presence: true
