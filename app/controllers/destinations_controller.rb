8432e747d0c8cc8f4db8ac1f81cfaf630246667e
wanjiku <annritawanjiku77@gmail.com>
2022-12-06 11:59:34 +0300

    setup get/destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,9 @@
 class DestinationsController < ApplicationController
+
+    
+    # GET /destinations
+    def index
+        destinations = Destination.all
+        render json: destinations
+    end
 end
