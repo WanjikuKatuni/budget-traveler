5be313ba103bfcd7a105ee9eddb3029b03adbb1d
wanjiku <annritawanjiku77@gmail.com>
2022-12-06 12:04:13 +0300

    setup get/destinations/:id

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -6,4 +6,14 @@ class DestinationsController < ApplicationController
         destinations = Destination.all
         render json: destinations
     end
+
+    # GET /destinations/:id
+    def show
+        destination = Destination.find_by(id: params[:id])
+        if destination
+            render json: destination
+        else
+            render json: {error: "Budget-destination not found"}, status: :not_found
+        end
+    end
 end
