563f2a9b35b5916db4c1e6a9de0ef8eb74a898b4
wanjiku <annritawanjiku77@gmail.com>
2022-12-08 22:30:00 +0300

    typo correction

--- client/src/components/Alldestinations.js
+++ client/src/components/Alldestinations.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 
 function Alldestinations() {
 
@@ -6,7 +6,7 @@ function Alldestinations() {
     const [destinations, setDestinations] = useState([])
 
     useEffect(()=> {
-        getALl();
+        getAll();
     }, [])
 
 
@@ -18,7 +18,26 @@ function Alldestinations() {
 
 
   return (
-    <div>Alldestinations</div>
+    <div>
+        <h3 className='header'> Top Destinations</h3>
+        <div className='row row-cols-1 row-cols-md-3 g-4 '>
+        {destinations.map((destination)=>{
+          return(
+                 <div key={destination.id} className='col'>
+                  <div  className='card'>
+                    <img src={destination.poster_url} className='card-img-top' alt={destination.destination_name}/>
+                    <div className='card-body'>
+                      <p className='card-text'>{destination.destination_name}</p>
+                    </div>
+                    <div className='card-footer'>
+                        <small className='text-muted'>{destination.likes} likes</small>
+                    </div>
+                  </div>
+                </div>
+                )
+              })}
+         </div>
+    </div>
   )
 }
 
