77821dc800d9d334607f973449be1af3265b5087
wanjiku <annritawanjiku77@gmail.com>
2022-12-08 22:15:14 +0300

    add react to rails

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,8 @@
-import React from "react";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Pages from "./components/Pages";
-import Featured from "./components/Featured";
-import Top from "./components/Top";
-import {useEffect, useState} from "react";
-
 
 function App() {
-
-  const [destinations, setDestinations] = useState([])
-
-  useEffect(()=> {
-    getTop();
-  }, [])
-
-
-  // get top rated destinations
-  const getTop = async()=> {
-      fetch(`/destinations`)
-      .then(res => res.json())
-      .then(setDestinations)
-  }
-
   return (
     <div className="App">
-
-      <BrowserRouter>
-      <Routes>
-        <Route path="/featured" element={<Featured destinations={destinations}/>} />
-        <Route path="/top" element={<Top destinations={destinations}/>} />
-        <Route path="/" element={<Pages/>} />
-      </Routes>
-      </BrowserRouter>
-
+    <h1>Hello</h1>
     </div>
   );
 }
