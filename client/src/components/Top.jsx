c782594db3cb09db345111b49194a13f9b3034ab
wanjiku <annritawanjiku77@gmail.com>
2022-12-08 06:16:20 +0300

    display top destinations

--- client/src/components/Top.jsx
+++ client/src/components/Top.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
+// import Card  from 'react-bootstrap/Card';
+// import Col from 'react-bootstrap';
+// import Row from 'react-bootstrap';
+// import styled from 'styled-components';
 
 function Top() {
 
@@ -12,7 +16,7 @@ function Top() {
 
   // get top rated destinations
   const getTop = async()=> {
-      fetch('/destinations')
+      fetch(`/destinations`)
       .then(res => res.json())
       .then(setDestinations)
 
@@ -25,20 +29,29 @@ function Top() {
 
   return (
     <div>
-
-      <h1> Top Destinations</h1>
-
-      {destinations.map((destination)=>{
-        return (
-          <div key={destination.id}>
-            <p>{destination.destination_name}</p>
-          </div>
-        )
-      })}
-
-
+      <h3> Top Destinations</h3>
+      <div className='row row-cols-1 row-cols-md-3 g-4 '>
+        {destinations.map((destination)=>{
+          return(
+                 <div key={destination.id} className='col'>
+                  <div  className='card'>
+                    <img src={destination.poster_url} className='card-img-top' alt={destination.destination_name}/>
+                    <div className='card-body'>
+                      <p className='card-text'>{destination.destination_name}</p>
+                    </div>
+                    <div className='card-footer'>
+                        <small className='text-muted'>{destination.likes} likes</small>
+                    </div>
+                  </div>
+                </div>
+                )
+              })}
+       </div>
     </div>
   )
 }
 
-export default Top
+
+export default Top;
+
+
