cded15c133c05c7fdd7ae79fc0a73c1fe91f3634
wanjiku <annritawanjiku77@gmail.com>
2022-12-09 15:04:51 +0300

    add login form component

---
+++ client/src/components/Login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Login({onLogin}) {
+
+    const [username, setUsername] = useState("");
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        fetch("/login",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username})
+        })
+        .then((res)=> res.json())
+        .then((user)=> onLogin(user))
+    }
+  return (
+    <form onSubmit={handleSubmit}>
+        <input type="text" value={username} onChange={(e)=> setUsername(e.target.value)}/>
+        <button type="submit" className="btn">Login</button>
+    </form>
+  )
+}
+
+export default Login
